29f908e4757ece7fecd6514aaf428c6cb73499d0
a01191 <85021240+a01191@users.noreply.github.com>
2021-06-08 12:38:28 +0800

    Update jd_cash.js

--- jd_cash.js
+++ jd_cash.js
@@ -29,7 +29,7 @@ let jdNotify = true;//是否关闭通知，false打开通知推送，true关闭�
 let cookiesArr = [], cookie = '', message;
 
 let helpAuthor = false;
-const randomCount = $.isNode() ? 20 : 5;
+const randomCount = $.isNode() ? 5 : 5;
 
 let cash_exchange = false;//是否消耗2元红包兑换200京豆，默认否
 const inviteCodes = [
