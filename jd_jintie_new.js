7dc4e773cf8f42cc78b19420cdb07697d9d55121
a01191 <85021240+a01191@users.noreply.github.com>
2021-06-10 15:04:16 +0800

    Update jd_jintie_new.js

--- jd_jintie_new.js
+++ jd_jintie_new.js
@@ -26,7 +26,7 @@ if ($.isNode()) {
     cookiesArr.push(jdCookieNode[item]);
   });
   if (process.env.JD_DEBUG && process.env.JD_DEBUG === "false") console.log = () => {};
-  if (JSON.stringify(process.env).indexOf('GITHUB') > -1) process.exit(0);
+  //if (JSON.stringify(process.env).indexOf('GITHUB') > -1) process.exit(0);
 } else {
   cookiesArr = [
     $.getdata("CookieJD"),
